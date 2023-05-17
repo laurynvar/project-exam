c6769dc76aedd4ed7874c42b7873846c54fbb541
laurynvar <laurynas.varkauskas@gmail.com>
2023-05-18 01:35:49 +0300

    Login component & route created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { HomePage } from "./components/Home";
+import { Login } from "./components/Login";
 
 function App() {
   return (
     <Routes>
       <Route element={<HomePage />} path="/" />
+      <Route element={<Login />} path="login" />
     </Routes>
   );
 }
