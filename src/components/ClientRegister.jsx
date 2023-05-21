bd48d3e2c7fcc5cc9733208f49f9287b19b1847b
laurynvar <laurynas.varkauskas@gmail.com>
2023-05-22 00:25:12 +0300

    ClientRegister form created

--- src/components/ClientRegister.jsx
+++ src/components/ClientRegister.jsx
@@ -1,3 +1,46 @@
+import React from "react";
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBInput,
+  MDBBtn,
+  MDBTextArea,
+} from "mdb-react-ui-kit";
+
 export const ClientRegister = () => {
-  return <div>This is client registration page</div>;
+  return (
+    <MDBContainer fluid className="mt-5">
+      <section>
+        <MDBRow className="justify-content-center">
+          <MDBCol lg="8">
+            <form>
+              <MDBRow>
+                <MDBCol>
+                  <MDBInput className="mb-4" label="First name" />
+                </MDBCol>
+                <MDBCol>
+                  <MDBInput className="mb-4" label="Last name" />
+                </MDBCol>
+              </MDBRow>
+
+              <MDBInput className="mb-4" label="Email" type="email" />
+
+              <MDBInput className="mb-4" label="Phone" type="tel" />
+
+              <MDBTextArea
+                label="Additional information"
+                rows={4}
+                className="mb-4"
+              />
+
+              <MDBBtn type="submit" block className="mb-4">
+                Register participant
+              </MDBBtn>
+            </form>
+          </MDBCol>
+        </MDBRow>
+      </section>
+    </MDBContainer>
+  );
 };
